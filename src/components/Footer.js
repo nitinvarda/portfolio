0975df04761d4c9771879bf410cea0ce4896f0de
nitin <nitinvardha09@gmail.com>
2021-02-21 22:00:38 +0530

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Particle from '../screens/Particle'
 
 function Footer() {
     return (
-        <div className='d-flex flex-row justify-content-center  p-4' style={{ backgroundColor: '#222' }}>
+        <div className='d-flex flex-row justify-content-center ' style={{ backgroundColor: '#222',height:'8vh',position:'relative' }}>
+            <Particle    />
+            <div style={{position:'absolute',top:10,display:'flex',flexDirection:'row',justifyContent:'center',width:'100%',color:'white'}}>
+
             <a href="mailto:nitinvardha09@gmail.com" style={{ textDecoration: 'none' }}><h6 className='text-white'>nitinvardha09@gmail.com</h6></a>
+            </div>
         </div>
     )
 }
